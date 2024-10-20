2ae0ab2c55aaea2140b0c587bb45eda62b5a8001
HugoAbisaiReyesTrejo-215201 <143544581+HugoAbisaiReyesTrejo-215201@users.noreply.github.com>
2024-10-20 14:42:45 -0600

    Vista de actualizacion de datos de envio finalizada y conexion a los botones

--- shipping/urls.py
+++ shipping/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.shipping, name='shipping'), # Ruta para la página principal
+    path('shipping_actualizar_datos/', views.shipping_actualizar_datos, name='shipping_actualizar_datos') # Ruta para la página principal
 ]
--- shipping/views.py
+++ shipping/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def shipping(request):
     return render(request, 'shipping/shipping.html')
+
+
+def shipping_actualizar_datos(request):
+    return render(request, 'shipping/shipping_actualizar_datos.html')
