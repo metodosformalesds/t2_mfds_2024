47b10abd807d7b6f3630b1ac77c8d5b7e5fa7a27
HugoAbisaiReyesTrejo-215201 <143544581+HugoAbisaiReyesTrejo-215201@users.noreply.github.com>
2024-11-06 22:43:59 -0600

    Integración de Google Maps: API de JavaScript y Geocoding

--- recycler/urls.py
+++ recycler/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recycler, name='recycler'), # Ruta para la página principal
+    path('', views.recycler, name='recycler'),  # Ruta para la página principal
+    path('map/', views.recycler_map_view, name='recycler_map'),  # Ruta para la vista del mapa
 ]
--- recycler/views.py
+++ recycler/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def recycler(request):
     return render(request, 'recycle.html')
+
+def recycler_map_view(request):
+    return render(request, 'map.html')
